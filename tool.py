30ce34782a6498b0159c6bb95f9356ace2580fab
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-25 11:58:34 -0500

    Add get_results method to tool module.

--- tool.py
+++ tool.py
@@ -1,3 +1,7 @@
+import os
+import subprocess
+import tempfile
+
 # Change PROG_NAME to path to wildebeest move generator executable
 PROG_NAME = "cat"
 
@@ -46,4 +50,51 @@ def is_valid(board):
 
     return True
 
+def get_results(board):
+    """
+    Runs the wildebeest move generating executable and returns results.
+    - Creates a temporary directory
+    - Saves board contents to file in temporary directory
+    - Runs the move generating executable on the board file
+    - Copies the generated files into memory
+    - Removes all generated files
+    - Removes temporary directory
+    - Returns list of contents of generated files
+    """
 
+    # Create a new temp directory
+    tmp_dir = tempfile.mkdtemp()
+    print tmp_dir
+
+    # Save board.txt to temp directory
+    board_filename = os.path.join(tmp_dir, 'board.txt')
+    with open(board_filename, 'w') as board_file:
+        board_file.write(board)
+
+    # Change current working directory to temp directory
+    orig_wd = os.curdir
+    os.chdir(tmp_dir)
+
+    # Call wildebeest move generating executable
+    ret_val = subprocess.call([PROG_NAME, board_filename])
+    if ret_val != 0:
+        return None
+
+    # Delete board.txt from temp directory
+    os.remove(board_filename)
+
+    # Get contents of new boards from temp directory and delete the files
+    new_boards = []
+    for new_board_filename in os.listdir(tmp_dir):
+        with open(new_board_filename) as new_board_file:
+            lines = [line for line in new_board_file]
+            new_boards.append(''.join(lines))
+        os.remove(new_board_filename)
+
+    # Change back to original working directory
+    os.chdir(orig_wd)
+
+    # Delete temp directory
+    os.rmdir(tmp_dir)
+
+    return new_boards
