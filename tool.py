f89af4be78cb7192e46eec30044c12abc4fc04bf
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-25 12:22:57 -0500

    Return invalid-reason from is_valid method

--- tool.py
+++ tool.py
@@ -13,6 +13,10 @@ def is_valid(board):
         line 1) 'W' or 'B'
         lines 2-9) 8 characters from VALID_PIECES list
         lines 10-12) integers
+
+    Returns:
+        - True, None if board was valid
+        - False, reason if board was invalid, where reason is a string
     """
     # Remove trailing new lines
     board = board.rstrip()
@@ -25,20 +29,20 @@ def is_valid(board):
 
     # Check that there are the correct number of lines in the board
     if len(lines) != 12:
-        return False
+        return False, "board config must be 12 lines long"
 
     # Check that first line is either 'W' or 'B'
     if lines[0] not in ('W', 'B'):
-        return False
+        return False, "first line must be either 'W' or 'B'"
     
     for line in lines[1:9]:
         # Check that the line is 8 characters long
         if len(line) != 8:
-            return False
+            return False, "board lines must be 8 characters wide"
 
         # Check that each line is only composed of valid pieces
         if any(c not in VALID_PIECES for c in line):
-            return False
+            return False, "board contained invalid piece"
 
     # Check that the last three lines are integers
     try:
@@ -46,9 +50,9 @@ def is_valid(board):
         int(lines[10])
         int(lines[11])
     except ValueError:
-        return False
+        return False, "last three lines must be valid integers"
 
-    return True
+    return True, None
 
 def get_results(board):
     """
